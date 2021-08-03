b19f8c4a11cc8fb31d0b7bc8472fec78a34ef6e3
Josefrancisco <antonio.j1.francisco@gmail.com>
2021-08-03 09:57:18 +0100

    terminando a destruição de projecto

--- home.php
+++ home.php
@@ -168,7 +168,7 @@ session_start();
 					
 					if(isset($_SESSION['sess_user_id']) && $_SESSION['sess_user_id'] != "") {
 						echo $_SESSION['sess_name'];
-						echo '<p><a href="logout.php">Sair</a></p>';
+						echo "<p><a href='sair.php'>Sair</a></p>";
 					} else { 
 						header('location:index.php');
 					}
